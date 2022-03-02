e35898315ab0f9c38c307603b4b5dcccedb2ff87
Hannah6898 <hannahosibodu68@outlook.com>
2022-03-02 18:09:07 +0000

    Added search bar functionality

--- src/App.js
+++ src/App.js
@@ -3,55 +3,39 @@ import "./App.css";
 import HomePage from "./Container//HomePage/HomePage";
 import Details from "./Container/Details/Details";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Header from "./Components/Header/Header";
 
 function App() {
-  const [data, setData] = useState();
-  const [search, setSearch] = useState();
+  const [data, setData] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+
+  const getMovieRequest=(searchValue)=>{
+    fetch(`http://www.omdbapi.com/?s=${searchValue}&apikey=ae3fd08d`)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      if (data.Search){
+        setData(data.Search);
+      }
+    });
+  }
 
   //Get data
   useEffect(() => {
-    fetch("http://www.omdbapi.com/?s=star wars&apikey=ae3fd08d")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setData(data.Search);
-      });
-  }, []);
+    getMovieRequest(searchValue);
+  }, [searchValue]);
 
-  // //Search bar
-  // function handleChange(event) {
-  //   // console.log(event.target.value)
-  //   setSearch(event.target.value);
-  //   console.log(search);
-  // }
-
-  // function searchMedia(e) {
-  //   e.preventDefault();
-  //   fetch(`http://img.omdbapi.com/?s=${search}&apikey=ae3fd08d`)
-  //     .then((res) => {
-  //       return res.json();
-  //     })
-  //     .then((searchData) => {
-  //       setData(searchData);
-  //       console.log(searchData);
-  //     });
-  // }
 
   return (
     <div className="App m-5">
+      <Header
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
       <Router>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <HomePage
-                data={data}
-                // handleChange={handleChange}
-                // searchMedia={searchMedia}
-              />
-            }
-          />
+          <Route path="/" element={<HomePage data={data} />} />
           <Route path="/details" element={<Details />} />
         </Routes>
       </Router>
@@ -74,3 +58,14 @@ export default App;
 //http://www.omdbapi.com/?apikey=ae3fd08d&i=tt1285016
 
 // data={data} handleChange={handleChange} searchMedia={searchMedia}
+
+
+
+// const getMovieRequest= async()=>{
+//   const url = 'http://www.omdbapi.com/?s=star wars&apikey=ae3fd08d';
+
+//   const response = await fetch(url);
+//   const responseJson = await response.json();
+
+//   console.log(responseJson)
+// }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,15 +5,18 @@ function Header(props) {
   return (
     <div>
       <header className={classes.header}>
+        <div className="row">
         <h1 className={classes.title}>Movie App</h1>
         <form onSubmit={props.searchMedia}>
           <input
             type="text"
             className={classes.input}
             placeholder="Search Movie, TV show, Actor, Director......."
-            onChange={props.handleChange}
+            onChange={(event)=>props.setSearchValue(event.target.value)}
+            value={props.searchValue}
           ></input>
         </form>
+        </div>
       </header>
     </div>
   );
