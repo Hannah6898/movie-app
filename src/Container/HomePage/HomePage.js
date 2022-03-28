0bb7951e05f74307ea7d4b9b0673791b85815a6b
Hannah6898 <hannahosibodu68@outlook.com>
2022-03-28 16:05:00 +0100

    Added spacing to Homepage & details page

--- src/Container/HomePage/HomePage.js
+++ src/Container/HomePage/HomePage.js
@@ -2,20 +2,25 @@ import React from "react";
 import ImgCarousel from "../../Components/Home/ImgCarousel/ImgCarousel";
 import Movie from "../../Components/Home/ImgCarousel/Movie/Movie";
 import Favourites from "../../Components/Home/ImgCarousel/Favourites/Favourites";
+import classes from "./HomePage.module.css"
 
 function HomePage(props) {
   return (
     <div>
+      <div className={classes.wrapper}>
       <ImgCarousel
         heading="Movies & TV "
         movie={
           <Movie data={props.data} getMovieDetails={props.getMovieDetails} />
         }
       />
+      </div>
+      <div className={classes.wrapper}>
       <ImgCarousel
         heading="Favourties"
         movie={<Favourites favourites={props.favourites} />}
       />
+    </div>
     </div>
   );
 }
