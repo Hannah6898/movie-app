5bdf6d7830833048c3bc279646ac985414344a48
Hannah6898 <hannahosibodu68@outlook.com>
2022-04-02 10:10:52 +0100

    Animation on movie images

--- src/Components/Details/GenreIcons/GenreIcons.js
+++ src/Components/Details/GenreIcons/GenreIcons.js
@@ -2,21 +2,17 @@ import React from "react";
 import classes from "./GenreIcons.module.css";
 
 function GenreIcons(props) {
-  return(<div></div>)
-  // console.log(props.details.Genre);
-  // let genre = props.details.Genre;
-  // const array = genre.split(",");
-  // console.log(array);
-
-  // if (array === undefined) {
-  //   return null;
-  // } else {
-  //   return array.map((genre, i) => (
-  //     <div className="m-1" key={i}>
-  //       <button className={classes.genre}>{genre}</button>
-  //     </div>
-  //   ));
-  // }
+  let genre = props.details.Genre;
+  const array = genre.split(",");
+  if (array === undefined) {
+    return null;
+  } else {
+    return array.map((genre, i) => (
+      <div className="m-1" key={i}>
+        <button className={classes.genre}>{genre}</button>
+      </div>
+    ));
+  }
 }
 
 export default GenreIcons;
